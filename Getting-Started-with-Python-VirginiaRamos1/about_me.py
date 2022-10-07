68ea129af20e493e5296f10ddba842d15f6fe872
Virginia <viramos1999@gmail.com>
2022-10-06 21:38:53 -0400

    completing first python activity files

---
+++ Getting-Started-with-Python-VirginiaRamos1/about_me.py
@@ -0,0 +1,14 @@
+full_name= "Virginia Ramos"
+age = 23
+has_used_python =True
+hobbies = ["drawing","reading","visiting coffee shops","backpacking"]
+fav_things = {
+  "movie": "under the same moon",
+  "food" : "chicken",
+  "hobby": "hiking",
+  "number": "14"
+  
+  
+}
+all_vars = dict(vars())
+
